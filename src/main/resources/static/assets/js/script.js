4ff5398f484cc7867f610634883187883ee536cd
bmanish55 <mbhavar143@gmail.com>
2025-08-16 00:56:12 +0530

    Updated Everthing

--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -22,3 +22,33 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(fill);
   });
 });
+
+var swiper = new Swiper(".swiper",{
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 2,
+  speed: 600,
+  preventClicks: false,
+  slidesPerView:"auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true,
+  },
+  on: {
+  click(event) {
+    const slide = this.slides[this.clickedIndex];
+    const url = slide.dataset.url;
+    if (url) {
+      window.open(url, "_blank");
+    }
+  },
+},
+  pagination: {
+    el: ".swiper-pagination",
+  }
+})
+
